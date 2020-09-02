32c8bf4a297a9fce9414509dc942863407b5d131
kykon13 <70661589+kykon13@users.noreply.github.com>
2020-09-02 21:11:18 +0200

    Add files via upload

---
+++ arkanoid.cpp
@@ -0,0 +1,189 @@
+//---------------------------------------------------------------------------
+
+#include <vcl.h>
+#pragma hdrstop
+
+#include "arkanoid.h"
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+#pragma resource "*.dfm"
+TForm1 *Form1;
+
+        int x=-8;
+        int y=-8;
+
+        int do_wygranej=19;
+
+        bool kolizja (TImage* pilka, TImage * cegla)
+        {
+             if(pilka->Left>=cegla->Left-pilka->Width &&
+                pilka->Left<=cegla->Left+cegla->Width &&
+                pilka->Top>=cegla->Top-pilka->Width &&
+                pilka->Top<=cegla->Top+cegla->Width )
+
+             {
+                return true;
+             }
+             else return false;
+        }
+
+//---------------------------------------------------------------------------
+__fastcall TForm1::TForm1(TComponent* Owner)
+        : TForm(Owner)
+{
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm1::Timer_pilkaTimer(TObject *Sender)
+{
+        b->Left+=x;
+        b->Top+=y;
+
+        //odbij od lewej sciany
+        if(b->Left-5<=tlo->Left) x=-x;
+
+        //odbij od górnej sciany
+        if(b->Top-5<=tlo->Top) y=-y;
+
+        //odbij od prawej sciany
+        if(b->Left+b->Width+5>=tlo->Width) x=-x;
+
+        //skucha!
+        if(b->Top >= p->Top+p -> Height+15)
+        {
+        Timer_pilka->Enabled=false;
+        b->Visible=false;
+        Button1->Caption="Pora¿ka! Jeszcze raz?";
+        Button1->Visible=true;
+
+        }  else if (b->Left > p->Left-b->Width/2 && b->Left+p->Width &&
+                        b->Top+b->Height>p->Top)
+        {
+             if (y>0) y=-y;
+        }
+
+
+        if(do_wygranej<=0)
+        {
+                Timer_pilka->Enabled=false;
+                b->Visible=false;
+                Button1->Caption="Wygrana! Jeszcze raz?";
+                Button1->Visible=true;
+        }
+
+        //blok
+        if(kolizja(b,Image)&&Image->Visible==true)
+        {x=-x; y=-y; Image->Visible=false; do_wygranej--;}
+        //blok1
+        if(kolizja(b,Image1)&&Image1->Visible==true)
+        {x=-x; y=-y; Image1->Visible=false; do_wygranej--;}
+        //blok2
+        if(kolizja(b,Image2)&&Image2->Visible==true)
+        {x=-x; y=-y; Image2->Visible=false; do_wygranej--;}
+        //blok3
+        if(kolizja(b,Image3)&&Image3->Visible==true)
+        {x=-x; y=-y; Image3->Visible=false; do_wygranej--;}
+        //blok4
+        if(kolizja(b,Image4)&&Image4->Visible==true)
+        {x=-x; y=-y; Image4->Visible=false; do_wygranej--;}
+        //blok5
+        if(kolizja(b,Image5)&&Image5->Visible==true)
+        {x=-x; y=-y; Image5->Visible=false; do_wygranej--;}
+        //blok6
+        if(kolizja(b,Image6)&&Image6->Visible==true)
+        {x=-x; y=-y; Image6->Visible=false; do_wygranej--;}
+        //blok7
+        if(kolizja(b,Image7)&&Image7->Visible==true)
+        {x=-x; y=-y; Image7->Visible=false; do_wygranej--;}
+        //blok8
+        if(kolizja(b,Image8)&&Image8->Visible==true)
+        {x=-x; y=-y; Image8->Visible=false; do_wygranej--;}
+        //blok9
+        if(kolizja(b,Image9)&&Image9->Visible==true)
+        {x=-x; y=-y; Image9->Visible=false; do_wygranej--;}
+        //blok10
+        if(kolizja(b,Image10)&&Image10->Visible==true)
+        {x=-x; y=-y; Image10->Visible=false; do_wygranej--;}
+        //blok11
+        if(kolizja(b,Image11)&&Image11->Visible==true)
+        {x=-x; y=-y; Image11->Visible=false; do_wygranej--;}
+        //blok12
+        if(kolizja(b,Image12)&&Image12->Visible==true)
+        {x=-x; y=-y; Image12->Visible=false; do_wygranej--;}
+        //blok13
+        if(kolizja(b,Image13)&&Image13->Visible==true)
+        {x=-x; y=-y; Image13->Visible=false; do_wygranej--;}
+        //blok14
+        if(kolizja(b,Image14)&&Image14->Visible==true)
+        {x=-x; y=-y; Image14->Visible=false; do_wygranej--;}
+        //blok15
+        if(kolizja(b,Image15)&&Image15->Visible==true)
+        {x=-x; y=-y; Image15->Visible=false; do_wygranej--;}
+        //blok16
+        if(kolizja(b,Image16)&&Image16->Visible==true)
+        {x=-x; y=-y; Image16->Visible=false; do_wygranej--;}
+        //blok17
+        if(kolizja(b,Image17)&&Image17->Visible==true)
+        {x=-x; y=-y; Image17->Visible=false; do_wygranej--;}
+        //blok18
+        if(kolizja(b,Image18)&&Image18->Visible==true)
+        {x=-x; y=-y; Image18->Visible=false; do_wygranej--;}
+
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm1::lewoTimer(TObject *Sender)
+{
+      if(p->Left > 10) p->Left -= 10;
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm1::prawoTimer(TObject *Sender)
+{
+      if(p->Left+p->Width < tlo->Width-10)  p->Left += 10;
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm1::FormKeyDown(TObject *Sender, WORD &Key,
+      TShiftState Shift)
+{
+        if(Key==VK_LEFT) lewo->Enabled=true;
+        if(Key==VK_RIGHT) prawo->Enabled=true;
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm1::FormKeyUp(TObject *Sender, WORD &Key,
+      TShiftState Shift)
+{
+        if(Key==VK_LEFT) lewo->Enabled=false;
+        if(Key==VK_RIGHT) prawo->Enabled=false;
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm1::Button1Click(TObject *Sender)
+{
+        b->Left=300;
+        b->Top=360;
+
+        b->Visible=true;
+        x=-8;y=-8;
+        Timer_pilka->Enabled=true;
+
+        Button1->Visible=false;
+        do_wygranej=19;
+
+        Image->Visible=true;
+        Image1->Visible=true;
+        Image2->Visible=true;
+        Image3->Visible=true;
+        Image4->Visible=true;
+        Image5->Visible=true;
+        Image6->Visible=true;
+        Image7->Visible=true;
+        Image8->Visible=true;
+        Image9->Visible=true;
+        Image10->Visible=true;
+        Image11->Visible=true;
+        Image12->Visible=true;
+        Image13->Visible=true;
+        Image14->Visible=true;
+        Image15->Visible=true;
+        Image16->Visible=true;
+        Image17->Visible=true;
+        Image18->Visible=true;
+}
+//---------------------------------------------------------------------------
