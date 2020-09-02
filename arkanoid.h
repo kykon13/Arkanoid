98df2f9181b124abb6bbfb62feb79b11beec4e33
kykon13 <70661589+kykon13@users.noreply.github.com>
2020-09-02 21:17:05 +0200

    Add files via upload

---
+++ arkanoid.h
@@ -0,0 +1,57 @@
+//---------------------------------------------------------------------------
+
+#ifndef arkanoidH
+#define arkanoidH
+//---------------------------------------------------------------------------
+#include <Classes.hpp>
+#include <Controls.hpp>
+#include <StdCtrls.hpp>
+#include <Forms.hpp>
+#include <ExtCtrls.hpp>
+#include <Graphics.hpp>
+//---------------------------------------------------------------------------
+class TForm1 : public TForm
+{
+__published:	// IDE-managed Components
+        TImage *b;
+        TTimer *Timer_pilka;
+        TImage *p;
+        TTimer *lewo;
+        TTimer *prawo;
+        TImage *Image;
+        TImage *Image1;
+        TImage *Image2;
+        TImage *Image3;
+        TImage *Image4;
+        TImage *Image5;
+        TImage *Image6;
+        TImage *Image7;
+        TImage *Image8;
+        TImage *Image9;
+        TImage *Image10;
+        TImage *Image11;
+        TImage *Image12;
+        TImage *Image13;
+        TImage *Image14;
+        TImage *Image15;
+        TImage *Image16;
+        TImage *Image17;
+        TImage *Image18;
+        TShape *tlo;
+        TButton *Button1;
+        void __fastcall Timer_pilkaTimer(TObject *Sender);
+        void __fastcall lewoTimer(TObject *Sender);
+        void __fastcall prawoTimer(TObject *Sender);
+        void __fastcall FormKeyDown(TObject *Sender, WORD &Key,
+          TShiftState Shift);
+        void __fastcall FormKeyUp(TObject *Sender, WORD &Key,
+          TShiftState Shift);
+        void __fastcall Button1Click(TObject *Sender);
+private:	// User declarations
+public:		// User declarations
+        __fastcall TForm1(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TForm1 *Form1;
+//---------------------------------------------------------------------------
+#endif
